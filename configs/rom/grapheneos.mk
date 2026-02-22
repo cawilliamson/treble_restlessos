8eee3e1fcdd2cc4d010acbb4948d246650e5a845
Christopher A. Williamson <contact@chrisaw.io>
2026-02-22 22:14:40 +0000

    Force 64bit zygote to fix multilib vanadium

--- configs/rom/grapheneos.mk
+++ configs/rom/grapheneos.mk
@@ -1,8 +1,2 @@
 # ROM Configuration for GrapheneOS builds
 # This file is copied to device/phh/treble/grapheneos.mk during the build process
-
-# Force 64-bit only zygote. GrapheneOS Vanadium (Trichrome WebView/Chrome)
-# only ships 64-bit native libraries. Without this, 32-bit apps on multilib
-# devices fail to load WebView because there are no armeabi-v7a libs in the
-# Trichrome APKs.
-ZYGOTE_FORCE_64 := true
