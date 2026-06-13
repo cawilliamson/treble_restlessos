a853b37a4058f436182826930b0e109a105eaaba
Christopher A. Williamson <contact@chrisaw.io>
2026-06-13 15:11:37 +0100

    chore(terraform): import existing AWS resources and configure S3 backend
    Import all existing GSI build infrastructure into OpenTofu state:
    - eu-west-2 VPC, subnets, IGW, route tables, security group
    - eu-west-1 VPC, subnets, IGW, route tables, security group
    - IAM user, policy, access key, EC2 spot service-linked role
    
    Also configure the S3-only remote backend with native state locking
    (use_lockfile = true) and create the state bucket with versioning
    and default encryption.
    
    The IAM policy was updated to include ec2:DeleteVolume, matching the
    latest config.

---
+++ .github/terraform/import.tf
@@ -0,0 +1,119 @@
+import {
+  to = aws_vpc.build
+  id = "vpc-01737f432ba3ff50c"
+}
+
+import {
+  to = aws_internet_gateway.build
+  id = "igw-035ed13dc0cff4152"
+}
+
+import {
+  to = aws_subnet.build["a"]
+  id = "subnet-05b4405ebd4eaf060"
+}
+
+import {
+  to = aws_subnet.build["b"]
+  id = "subnet-0fc99f8ce97ad502a"
+}
+
+import {
+  to = aws_subnet.build["c"]
+  id = "subnet-00a9ea820ac60a0c0"
+}
+
+import {
+  to = aws_route_table.build
+  id = "rtb-0653804d3a6f242f5"
+}
+
+import {
+  to = aws_route_table_association.build["a"]
+  id = "subnet-05b4405ebd4eaf060/rtb-0653804d3a6f242f5"
+}
+
+import {
+  to = aws_route_table_association.build["b"]
+  id = "subnet-0fc99f8ce97ad502a/rtb-0653804d3a6f242f5"
+}
+
+import {
+  to = aws_route_table_association.build["c"]
+  id = "subnet-00a9ea820ac60a0c0/rtb-0653804d3a6f242f5"
+}
+
+import {
+  to = aws_security_group.build
+  id = "sg-072d86797a735bfe3"
+}
+
+import {
+  to = aws_vpc.build_ireland
+  id = "vpc-0e6c094e3beb88480"
+}
+
+import {
+  to = aws_internet_gateway.build_ireland
+  id = "igw-086c653013bd8a7ff"
+}
+
+import {
+  to = aws_subnet.build_ireland["a"]
+  id = "subnet-07cbbebf71437cf82"
+}
+
+import {
+  to = aws_subnet.build_ireland["b"]
+  id = "subnet-0455ffe5942e72c66"
+}
+
+import {
+  to = aws_subnet.build_ireland["c"]
+  id = "subnet-0d061b649309bb70a"
+}
+
+import {
+  to = aws_route_table.build_ireland
+  id = "rtb-0f29bc668b29fe7f3"
+}
+
+import {
+  to = aws_route_table_association.build_ireland["a"]
+  id = "subnet-07cbbebf71437cf82/rtb-0f29bc668b29fe7f3"
+}
+
+import {
+  to = aws_route_table_association.build_ireland["b"]
+  id = "subnet-0455ffe5942e72c66/rtb-0f29bc668b29fe7f3"
+}
+
+import {
+  to = aws_route_table_association.build_ireland["c"]
+  id = "subnet-0d061b649309bb70a/rtb-0f29bc668b29fe7f3"
+}
+
+import {
+  to = aws_security_group.build_ireland
+  id = "sg-0e9e3c101891e227d"
+}
+
+import {
+  to = aws_iam_service_linked_role.spot
+  id = "arn:aws:iam::690475792081:role/aws-service-role/spot.amazonaws.com/AWSServiceRoleForEC2Spot"
+}
+
+import {
+  to = aws_iam_user.gha
+  id = "gsi-builder-gha"
+}
+
+import {
+  to = aws_iam_user_policy.gha_ec2
+  id = "gsi-builder-gha:ec2-spot-build"
+}
+
+import {
+  to = aws_iam_access_key.gha
+  id = "AKIA2BQ4QO3IXA73K6TK"
+}
