9fe666ff41fdf3d75c4ddcc167880263cb98e14c
Christopher A. Williamson <contact@chrisaw.io>
2026-06-17 21:36:30 +0100

    ci(terraform): remove duplicate subnet_id output from capacity.tf

--- .github/terraform/capacity.tf
+++ .github/terraform/capacity.tf
@@ -18,8 +18,3 @@ EOF
     instance_type = var.large_instance_type
   }
 }
-
-output "subnet_id" {
-  description = "subnet in the AZ with the best spot placement score"
-  value       = endswith(data.external.spot_score.result.az, "a") ? aws_subnet.build_a.id : endswith(data.external.spot_score.result.az, "b") ? aws_subnet.build_b.id : aws_subnet.build_c.id
-}
